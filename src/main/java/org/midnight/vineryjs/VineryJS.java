deb8754fed3716d0c8b14318a90054a264c79691
Rosie <treethatisorange@gmail.com>
2026-05-06 19:13:15 -0700

    fixing some things

---
+++ src/main/java/org/midnight/vineryjs/VineryJS.java
@@ -0,0 +1,13 @@
+package org.midnight.vineryjs;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(VineryJS.MOD_ID)
+public class VineryJS {
+    public static final String MOD_ID = "vineryjs";
+
+    public VineryJS(IEventBus modEventBus) {
+
+    }
+}
---
+++ src/main/java/org/midnight/vineryjs/VineryJSPlugin.java
@@ -0,0 +1,9 @@
+package org.midnight.vineryjs;
+
+import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.event.EventGroup;
+import dev.latvian.mods.kubejs.event.EventHandler;
+
+public class VineryJSPlugin extends KubeJSPlugin {
+
+}
