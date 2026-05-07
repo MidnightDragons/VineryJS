3cfa3dd6be0802a70d5a757302b868353d3e5dce
Rosie <treethatisorange@gmail.com>
2026-05-06 20:35:50 -0700

    Fixing access to a variable

--- src/main/java/org/midnight/vineryjs/mixin/DrinkBlockItemMixin.java
+++ src/main/java/org/midnight/vineryjs/mixin/DrinkBlockItemMixin.java
@@ -1,9 +1,10 @@
 package org.midnight.vineryjs.mixin;
 
 import net.satisfy.vinery.core.item.DrinkBlockItem;
+import org.midnight.vineryjs.duck.DrinkBlockItemDuck;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.midnight.vineryjs.duck.DrinkBlockItemDuck;
 
 @Mixin(DrinkBlockItem.class)
 public class DrinkBlockItemMixin implements DrinkBlockItemDuck {
